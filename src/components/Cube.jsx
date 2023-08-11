d76b440ea6d6fd6269927bf76305ea270b641e74
Amir <sammasovamir29@gmail.com>
2023-08-12 01:03:11 +0300

    Basic game

---
+++ src/components/Cube.jsx
@@ -0,0 +1,64 @@
+import React from "react"
+import { useBox } from "@react-three/cannon"
+import * as textures from "../images/textures"
+import { useStore } from "../hooks/useStore"
+
+const Cube = ({ position, texture }) => {
+  const [ref] = useBox(() => ({
+    type: "Static",
+    position,
+  }))
+
+  const [addCube, removeCube] = useStore((state) => [
+    state.addCube,
+    state.removeCube,
+  ])
+
+  const activeTexture = textures[texture + "Texture"]
+
+  return (
+    <mesh
+      ref={ref}
+      onClick={(e) => {
+        e.stopPropagation()
+        const clickedFace = Math.floor(e.faceIndex / 2)
+        const { x, y, z } = ref.current.position
+
+        if (e.shiftKey) {
+          console.log(e)
+          removeCube(x, y, z)
+          return
+        } else {
+          switch (clickedFace) {
+            case 0:
+              addCube(x + 1, y, z)
+              break
+            case 1:
+              addCube(x - 1, y, z)
+              break
+            case 2:
+              addCube(x, y + 1, z)
+              break
+            case 3:
+              addCube(x, y - 1, z)
+              break
+            case 4:
+              addCube(x, y, z + 1)
+              break
+            case 5:
+              addCube(x, y, z - 1)
+              break
+          }
+          return
+        }
+
+        console.log(clickedFace)
+      }}
+    >
+      <boxBufferGeometry attach="geometry" />
+      <meshStandardMaterial map={activeTexture} attach="material" />
+    </mesh>
+  )
+}
+
+export default Cube
